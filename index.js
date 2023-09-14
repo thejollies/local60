746e1d78d4ac2d3ebf435ea63421607ab0f60460
Sure, Not <13259903+nestak@users.noreply.github.com>
2023-09-14 15:35:06 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ const server = http.createServer((req, res) => {
   res.end('Hello World #2');
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
